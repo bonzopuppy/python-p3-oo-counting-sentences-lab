2496e6770a0050ab63746e0434ba18fc7a5010ee
David Baden <davebaden@gmail.com>
2023-12-28 12:30:50 -0800

    pass tests

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
+import re
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = value if isinstance(value, str) else ''
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return self._value.endswith('.')
+
+    def is_question(self):
+        return self._value.endswith('?')
+
+    def is_exclamation(self):
+        return self._value.endswith('!')
+
+    def count_sentences(self):
+        pattern = r'[\.\?!]+'
+        matches = re.findall(pattern, self._value)
+
+        return len(matches)
